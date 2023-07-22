8abd2d8c59252974be0e23363a3d0484d31eadce
Sebastian Satelier <sebastian.satelier@gmail.com>
2023-07-21 21:55:37 -0300

    first commit

---
+++ src/com/ss/conversorMultiple/Logica/ConversorMultiple.java
@@ -0,0 +1,11 @@
+package com.ss.conversorMultiple.Logica;
+
+
+public class ConversorMultiple {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
